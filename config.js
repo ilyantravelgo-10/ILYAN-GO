4c330ecd18e5348efa69131e4593fabbaf803de4
ilyantravelgo-10 <ilyantravelgo@gmail.com>
2026-07-11 19:12:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-// ILYAN GO - Configuration de la base de données Supabase / إعدادات قاعدة بيانات سوبابيس
-// تم ربط لوحة التحكم بقاعدة بيانات Supabase بشكل كامل ونهائي.
-// أي تعديل من لوحة التحكم سيظهر فوراً على الموقع المنشور.
-
-window.SUPABASE_CONFIG = {
-    url: "https://jtajdmrezokuqzvymlux.supabase.co",
-    key: "sb_publishable_Dp1NXlNXNGynqpkro0TKaA_9SO14aFA",
-    active: true
-};
+window.SUPABASE_CONFIG={url:"https://jtajdmrezokuqzvymlux.supabase.co",key:"sb_publishable_Dp1NXlNXNGynqpkro0TKaA_9SO14aFA",active:true};
