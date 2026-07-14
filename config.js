860143209b3775c365a0c880dcbc3dd89f106f22
ilyantravelgo-10 <ilyantravelgo@gmail.com>
2026-07-14 14:09:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
-  url: "https://XXXX.supabase.co",
-  key: "sb_publishable_...",
+  url: "https://jtajdmrezokuqzvymlux.supabase.co/rest/v1/",
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0YWpkbXJlem9rdXF6dnltbHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3MjA3MDAsImV4cCI6MjA5OTI5NjcwMH0.wDvNfL_JbomkDSXA29cX_L0AD68O8YkW0_n0vGpGIUk",
   active: true
 };
